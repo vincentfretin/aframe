f7c762c29f8dae83723e6a2e253e34e8ce7cf08b
Vincent Fretin <vincent.fretin@gmail.com>
2026-03-30 19:30:23 +0200

    Build dist

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ if (utils.device.isBrowserEnvironment) {
   require('./style/aframe.css');
 }
 
-console.log('A-Frame Version: 1.7.1 (Date 2026-03-28, Commit #e9ac2807)');
+console.log('A-Frame Version: 1.7.1 (Date 2026-03-30, Commit #aa5ceabd)');
 console.log('THREE Version (https://github.com/supermedium/three.js):',
             THREE.REVISION);
 
